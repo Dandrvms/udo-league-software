0da9d5e782b20fd60fc8bb58e86c0589255f565f
PedroCabrera <kabreraponte@gmail.com>
2025-06-08 01:42:29 -0400

    Actualizacion - Pedro

--- src/app/administrador/campeonato/[id]/categoria/[idCategoria]/equipos/cargar/page.jsx
+++ src/app/administrador/campeonato/[id]/categoria/[idCategoria]/equipos/cargar/page.jsx
@@ -45,7 +45,7 @@ export default async function CargarEquiposPage({ params }) {
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                                             </svg>
-                                            <a href="/plantilla-equipos.csv" download className="hover:underline">
+                                            <a href="/plantilla-equipos.xlsx" download className="hover:underline">
                                                 Descargar plantilla de ejemplo
                                             </a>
                                         </div>
